e7d9983ffbccc748ea63d29f0db0b465252bfa48
Denis <snytko_d@mail.ru>
2023-03-19 19:51:40 +0300

    123

--- scripts/index.js
+++ scripts/index.js
@@ -20,14 +20,49 @@ function popupClosed() {
   popup.classList.remove('popup_opened');
 }
 
-popupCloseButton.addEventListener('click', popupClosed);
+/*Открытие попапа с увеличенным изображением*/
+function handleFormImage (cardData) {
+  pictureImage.src = cardData.link;
+  pictureImage.alt = cardData.name.slice(0, 1).toUpperCase() + cardData.name.slice(1);
+  titleImageText.textContent = cardData.name.slice(0, 1).toUpperCase() + cardData.name.slice(1);
+  openPopup(imagePopup);
+};
 
+function createCard (cardData) {
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  const cardImage = cardElement.querySelector('.card__image');
+  const cardTitle = cardElement.querySelector('.card__heading');
+  const buttonLike = cardElement.querySelector('.card__like-button');
+  const buttonRemove = cardElement.querySelector('.card__remove-button');
+  cardImage.src = cardData.link;
+  cardImage.alt = cardData.name.slice(0, 1).toUpperCase() + cardData.name.slice(1);
+  cardTitle.textContent = cardData.name.slice(0, 1).toUpperCase() + cardData.name.slice(1);
+  buttonLike.addEventListener('click', toggleLikeCard);
+  buttonRemove.addEventListener('click', removeCard);
+  cardImage.addEventListener('click', () => handleFormImage(cardData));
+  console.log('123');
+  return cardElement;
+};
 
-function handleFormSubmit(evt) {
+/*функция добавления данных карточки в разметку*/
+function addCardFormSubmit (evt) {
   evt.preventDefault();
-  popupClosed();
-  profileName.textContent = profileNameInput.value;
-  profileDescription.textContent = profileDescriptionInput.value;
-}
 
-formElement.addEventListener('submit', handleFormSubmit);
+  const newCard = createCard({
+    name: titleInput.value,
+    link: hrefInput.value
+  });
+
+  cardContainer.prepend(newCard);
+
+  closePopup(formCardPopup);
+
+  formCard.reset();
+};
+
+formCard.addEventListener('submit', addCardFormSubmit);
+
+initialCards.forEach((cardData) => {
+  const newCardFromInitialCards = createCard (cardData);
+  cardContainer.append(newCardFromInitialCards);
+});
