3d70eb02ffc409776ef8bb0022b82a79511aad2e
Denis <snytko_d@mail.ru>
2023-03-20 15:32:21 +0300

    fixing BEM

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,21 @@
 const buttonOpenPopup = document.querySelector('.profile__edit-button');  //
 const popup = document.querySelector('.popup');  //
-const formElement = document.querySelector('.popup__profile'); //
-const formProfile = formElement.querySelector('.form__profile'); //
+const formElement = document.querySelector('.popup_popup_profile'); //
+const formProfile = formElement.querySelector('.form_form_profile'); //
 const nameInput = document.querySelector('#name'); //
 const jobInput = document.querySelector('#about'); //
 const nameText = document.querySelector('.profile__name-text'); //
 const jobText = document.querySelector('.profile__description'); //
-const formCardPopup = document.querySelector('.popup__cards'); //
+const formCardPopup = document.querySelector('.popup_popup_cards'); //
 const buttonOpenCardPopup = document.querySelector('.profile__add-button'); //
-const formCard = formCardPopup.querySelector('.form__cards'); //
+const formCard = formCardPopup.querySelector('.form_form_cards'); //
 const titleInput = formCardPopup.querySelector('.form__input-name'); //
 const hrefInput = formCardPopup.querySelector('.form__input-img'); //
 const cardContainer = document.querySelector('.cards__list'); //
 const closeButtons = document.querySelectorAll('.popup__close-button'); //
-const imagePopup = document.querySelector('.popup__image'); //
+const imagePopup = document.querySelector('.popup_popup_image'); //
 const pictureImage = imagePopup.querySelector('.popup__picture'); //
-const titleImageText = imagePopup.querySelector('.popup__title_image'); //
+const titleImageText = imagePopup.querySelector('.popup_popup_title_image'); //
 const cardTemplate = document.querySelector('#card-template').content; //
 
 function closePopup (popup) {
